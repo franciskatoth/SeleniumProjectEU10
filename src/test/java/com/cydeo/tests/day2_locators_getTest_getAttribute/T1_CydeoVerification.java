3161cab0d98d85b060f8bab85cff3ce8d83eced7
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2022-12-20 21:44:13 -0800

    First Step

--- src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/T1_CydeoVerification.java
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/T1_CydeoVerification.java
@@ -1,11 +1,19 @@
 package com.cydeo.tests.day2_locators_getTest_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerification {
     public static void main(String[] args) {
 
       //  TC #1: Cydeo practice tool verifications
        // 1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
        // 2. Go to https://practice.cydeo.com
+
        // 3. Verify URL contains
        // Expected: cydeo
        // 4. Verify title:
