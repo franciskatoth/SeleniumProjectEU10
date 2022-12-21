9bd3245a30505d192b061773e6532e2b35b91e0d
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2022-12-20 21:42:00 -0800

    Cydeo Verification

---
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/T1_CydeoVerification.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day2_locators_getTest_getAttribute;
+
+public class T1_CydeoVerification {
+    public static void main(String[] args) {
+
+      //  TC #1: Cydeo practice tool verifications
+       // 1. Open Chrome browser
+       // 2. Go to https://practice.cydeo.com
+       // 3. Verify URL contains
+       // Expected: cydeo
+       // 4. Verify title:
+     //   Expected: Practice
+
+
+
+    }
+}
