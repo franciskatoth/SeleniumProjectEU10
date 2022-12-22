3ded76df670d6f488f96b5a72b8e25f6299cca91
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2022-12-22 10:37:50 -0800

    Completed TC005

--- src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/TC005.java
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/TC005.java
@@ -2,6 +2,9 @@ package com.cydeo.tests.day2_locators_getTest_getAttribute;
 
 public class TC005 {
     // Work 1 completed
-    // wORK 2 COMPLETED
+
+    // WORK 2 COMPLETED
+
+    // TEST CASE FINISHED
 
 }
