0ce96c291c084f1e500909e63cb4fa231b7054b7
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2022-12-22 09:45:53 -0800

    Part 1 for T3

--- src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttribute/T3_GoogleSearch.java
@@ -1,11 +1,20 @@
 package com.cydeo.tests.day2_locators_getTest_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T3_GoogleSearch {
     public static void main(String[] args) {
 
 
        // TC#3: Google search
-       //  1- Open a chrome browser
+       //  1- Open a Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
        // 2- Go to: https://google.com
        // 3- Write “apple” in search box
        // 4- Click google search button
